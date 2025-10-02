bcf834736f3835c6478dd04f38222f618f08d674
ardagencer1206 <73840764+ardagencer1206@users.noreply.github.com>
2025-10-02 21:12:13 +0300

    Update backend.py

--- backend.py
+++ backend.py
@@ -17,7 +17,7 @@ app = Flask(__name__, static_url_path="", static_folder=".")
 AZURE_OPENAI_API_KEY = os.environ.get("AZURE_OPENAI_API_KEY", "d0167637046c4443badc4920cc612abb")
 AZURE_OPENAI_ENDPOINT = os.environ.get("AZURE_OPENAI_ENDPOINT", "https://openai-fnss.openai.azure.com")
 # Azure portalda oluşturduğun "Deployment name"
-AZURE_DEPLOYMENT_NAME = os.environ.get("AZURE_DEPLOYMENT_NAME", "gpt-4o-mini")  # <-- kendi deployment adını yaz
+AZURE_DEPLOYMENT_NAME = os.environ.get("AZURE_DEPLOYMENT_NAME", "gpt-4o")  # <-- kendi deployment adını yaz
 
 # API versiyonu: hesabındaki modele uygun bir sürüm olmalı (Genelde 2024-xx)
 AZURE_OPENAI_API_VERSION = os.environ.get("AZURE_OPENAI_API_VERSION", "2024-06-01")
@@ -601,6 +601,7 @@ def solve():
 if __name__ == "__main__":
     # Lokal test için:
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
